30d8549b376ff375eba28f7468c3b49d17129b69
Ven0msneak <122972226+Ven0msneak@users.noreply.github.com>
2024-07-16 19:02:06 +0800

    Update starter.js

--- starter.js
+++ starter.js
@@ -1,1 +1,17 @@
-const starters = ['Bulbasaur', 'Charmander', 'Squirtle'];
+const pokemon = [
+    { 
+        name: "Bulbasaur",
+        src: "Pokemon/qwilfish.png",
+        skills: ["Water Gun", "Poison Sting", "Spikes"]
+    },
+    { 
+        name: "Charmander",
+        src: "Pokemon/diglett.png",
+        skills: ["Dig", "Magnitude", "Sucker Punch"]
+    },
+    { 
+        name: "Squirtle",
+        src: "Pokemon/shelmet.png",
+        skills: ["Protect", "Bug Bite", "Acid Armor"]
+    },
+];
