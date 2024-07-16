ecf30b8c4ead6607dc53070eda22ab0ab5b2fcbe
Ven0msneak <122972226+Ven0msneak@users.noreply.github.com>
2024-07-16 19:11:49 +0800

    Update starter.js

--- starter.js
+++ starter.js
@@ -15,3 +15,21 @@ const pokemon = [
         skills: ["Protect", "Bug Bite", "Acid Armor"]
     },
 ];
+
+function choosePokemon(pokemonName)
+{
+var selectedPokemon = pokemon.find ( poke => poke.name ==== pokemonName );
+if (selectedPokemon)
+{
+    localStorage.setItem('starterPokemon', JSON.stringify(selectedPokemon);
+
+    selectedPokemonElement.innerHTML = `
+      <h2>Selected Pokémon: ${selectedPokemon.name}</h2>
+      <img src="${selectedPokemon.src}" alt="${selectedPokemon.name}">
+      <h3>Skills:</h3>
+      <ul>
+        ${selectedPokemon.skills.map(skill => `<li>${skill}</li>`).join('')}
+      </ul>
+    `;
+}
+}
